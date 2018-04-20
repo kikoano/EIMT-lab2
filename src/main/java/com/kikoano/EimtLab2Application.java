b6cf0b4f635538b2ee7854caca029a94c328b8d1
kikoano <kikoano111@yahoo.com>
2018-04-20 04:03:59 +0200

    Created project

---
+++ src/main/java/com/kikoano/EimtLab2Application.java
@@ -0,0 +1,12 @@
+package com.kikoano;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EimtLab2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EimtLab2Application.class, args);
+	}
+}
